fbbb3f9ca7a6f3adb114d141ce10045eb83e1453
Stephan-MDD <stephan.mdd@gmail.com>
2020-11-12 15:37:01 +0100

    added bin

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 // libraries
 import express from 'express';
 
+import IContract from 'contract';
+import {} from 'contract/src/dto/airport';
+
 // classes, interfaces & functions
 import ContractRPC from './contract/ContractRPC';
 
